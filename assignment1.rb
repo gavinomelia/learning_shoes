ed128fd5a2691da1ae1f1b2d78cbeb313e2d8fd2
Gavin OMelia <gavin@omelia.org>
2010-05-17 22:19:27 -0400

    Initial commit

---
+++ assignment1.rb
@@ -0,0 +1,19 @@
+Shoes.app :width => 800, :height => 800 do
+ 
+  background black
+
+  stroke green
+
+  oval :left=> 100, :top=> 100, :width => 600, :height=> 600
+
+oval :left=> 280, :top=> 150, :width => 70, :height=> 70
+
+oval :left=> 460, :top=> 150, :width => 70, :height=> 70
+
+fill green
+star :left => 410, :top => 350, :points => 5, :outer => 50, :inner => 30
+
+rect :left => 320, :top => 500, :width => 200, :height => 100
+
+
+end
---
+++ learning1.rb
@@ -0,0 +1,20 @@
+
+Shoes.app do
+
+ background rgb(0,0,0)                      
+ fill green..red     
+  stroke green
+     
+     fill rgb(255,255,255)
+     rect :left => 33, :top => 232, :width => 66, :height => 120
+       
+     oval :left => 60, :top => 15, :width => 50, :height => 200
+    
+     fill green..yellow
+     arrow :left => 40, :top => 100, :width => 100
+    
+     fill cyan
+     star :left => 200, :top => 200, :points => 5
+    
+ 
+end
