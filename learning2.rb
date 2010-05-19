2ec12da3beba5fe17fadade0c0c639b15b14f4d9
Gavin OMelia <gavin@omelia.org>
2010-05-19 08:44:22 -0400

    More learning

--- learning2.rb
+++ learning2.rb
@@ -1,11 +1,12 @@
 Shoes.app do
   para "Which kid:"
   which_kid_label = para("None selected yet")
-
-  list_of_kids = list_box(:items => [ "", "Madeline", "Gavin", "Keagan", "Luke", "Gabby", "Makinley"])
-
+  
+  list_of_kids = list_box(:items => [ "", "Madeline", "Gavin", "Keagan", "Luke", "Gabby", "Makinley", "Duff", "Kelly" ])
+  
   list_of_kids.change do |list|
-    new_label_value = "Yay!  You selected " + list.text
+    alert "Nice job changing that value!"
+    new_label_value = "yay!  You selected " + list.text
     which_kid_label.text = new_label_value
   end
   
